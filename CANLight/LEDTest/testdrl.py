0d49847232614737f7f0efbed950c20c59cf3c46
arkad94 <sahnivyom@gmail.com>
2024-02-01 19:18:31 +0530

    drlbriteadjust

--- CANLight/LEDTest/testdrl.py
+++ CANLight/LEDTest/testdrl.py
@@ -6,7 +6,7 @@ LED_COUNT = 16        # Number of LED pixels (4x4 grid).
 LED_PIN = 18          # GPIO pin connected to the pixels (must support PWM).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800kHz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 25   # Set to a low number for safety.
+LED_BRIGHTNESS = 15   # Set to a low number for safety.
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 
 # Create PixelStrip object with appropriate configuration
